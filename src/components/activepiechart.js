3a575cbb471530d2524be518d1e7c0aa8892a937
Bea Malsky <beamalsky@datamade.us>
2020-04-05 20:52:27 -0500

    Adjust pie chart display

--- src/components/activepiechart.js
+++ src/components/activepiechart.js
@@ -20,7 +20,8 @@ const renderActiveShape = (props) => {
 
   return (
     <g>
-      <text x={cx} y={cy} dy={8} textAnchor="middle">{payload.name}</text>
+      <text x={cx} y={cy} textAnchor="middle" fontWeight="bold">{payload.name}</text>
+      <text x={cx} y={cy} dy={18} textAnchor="middle">{`${value} Deaths`}</text>
       <Sector
         cx={cx}
         cy={cy}
@@ -41,8 +42,7 @@ const renderActiveShape = (props) => {
       />
       <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
       <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
-      <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">{`${value} Deaths`}</text>
-      <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
+      <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#999">
         {`${(percent * 100).toFixed(2)}%`}
       </text>
     </g>
