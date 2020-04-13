4152e54d3f2983563aba30270e4b8ac8442cac97
Bea Malsky <beamalsky@datamade.us>
2020-04-12 21:02:06 -0500

    Fix date parsing

--- src/pages/index.js
+++ src/pages/index.js
@@ -79,7 +79,9 @@ const getCVDataByDate = (data) => {
   dataCV = Object.entries(dataCV).map(
     obj => {
       var date = new Date(Date.parse(obj[0]))
-      var date_processed = date.toLocaleString('default', { month: 'long', day: 'numeric' })
+      // date parsing is off by one. correct is here
+      date.setDate(date.getDate() + 1)
+      var date_processed = (date).toLocaleString('default', { month: 'long', day: 'numeric' })
       return {
         'day': date_processed,
         'Reported deaths': obj[1]
