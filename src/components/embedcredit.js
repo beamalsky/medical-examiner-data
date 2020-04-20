992ff9aba6712072c30dffee536f8613a490048e
Bea Malsky <beamalsky@datamade.us>
2020-04-20 16:40:03 -0500

    Adjust byline width 2

--- src/components/embedcredit.js
+++ src/components/embedcredit.js
@@ -3,8 +3,8 @@ import { Col, Row } from 'react-bootstrap'
 
 const EmbedCredit = (props) => {
   return (
-    <Row className="justify-content-md-center">
-      <Col md={8} style={{ lineHeight: 1 }}>
+    <Row>
+      <Col lg={8} style={{ lineHeight: 1 }}>
         <small>
           Bea Malsky for <a href="https://southsideweekly.com/">South Side Weekly</a>. Last updated <b>{props.last_updated}</b> with data from the Cook County Medical Examiner's office. See the full live tracker at <a href="https://covid19neighborhoods.southsideweekly.com">covid19neighborhoods.southsideweekly.com</a>
         </small>
