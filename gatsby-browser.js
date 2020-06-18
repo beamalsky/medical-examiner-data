35d8dbda188655738fb231827908333cbe8b0376
pjsier <pjsier@gmail.com>
2020-06-17 22:34:07 -0400

    Setup pym.js for responsive iframes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,11 @@
 import React from "react"
+import * as pym from "pym.js"
 import { VizProvider } from "./src/context/vizcontext"
+
+export const onInitialClientRender = () => {
+  const pymChild = new pym.Child({ polling: 500 })
+  pymChild.sendHeight()
+}
 
 export const wrapRootElement = ({ element }) => (
   <VizProvider>{element}</VizProvider>
